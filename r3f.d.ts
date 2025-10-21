f1144dfff587f4661321c6823ca8d7e7707b3741
Latiffhl <latiffalikal@gmail.com>
2025-10-22 06:18:37 +0700

    add r3f

---
+++ r3f.d.ts
@@ -0,0 +1,11 @@
+// r3f.d.ts
+import { MeshLine, MeshLineMaterial } from 'meshline';
+import { Object3DNode, extend } from '@react-three/fiber';
+
+declare module '@react-three/fiber' {
+  interface ThreeElements {
+    meshLineGeometry: Object3DNode<MeshLine, typeof MeshLine>;
+
+    meshLineMaterial: Object3DNode<MeshLineMaterial, typeof MeshLineMaterial>;
+  }
+}
