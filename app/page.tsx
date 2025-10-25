410fab01498003a4f9042ca9792df8e726f8bebe
Latiffhl <latiffalikal@gmail.com>
2025-10-25 16:48:30 +0700

    add LogoLoopComponent

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,22 @@ import Squares from './components/Squares/Squares';
 import TargetCursor from './components/TargetCursor/TargetCursor';
 import GradientText from './components/GradientText/GradientText';
 import StickerPeel from './components/StickerPeel/StickerPeel';
+import LogoLoop from './components/LogoLoop/LogoLoop';
+
+import { SiReact, SiNextdotjs, SiTypescript, SiTailwindcss } from 'react-icons/si';
+
+const techLogos = [
+  { node: <SiReact />, title: 'React', href: 'https://react.dev' },
+  { node: <SiNextdotjs />, title: 'Next.js', href: 'https://nextjs.org' },
+  { node: <SiTypescript />, title: 'TypeScript', href: 'https://www.typescriptlang.org' },
+  { node: <SiTailwindcss />, title: 'Tailwind CSS', href: 'https://tailwindcss.com' },
+];
+// Alternative with image sources
+const imageLogos = [
+  { src: '/logos/company1.png', alt: 'Company 1', href: 'https://company1.com' },
+  { src: '/logos/company2.png', alt: 'Company 2', href: 'https://company2.com' },
+  { src: '/logos/company3.png', alt: 'Company 3', href: 'https://company3.com' },
+];
 
 export default function Home() {
   return (
@@ -105,6 +121,9 @@ export default function Home() {
             />
           </div>
         </div>
+      </div>
+      <div style={{ height: '200px', position: 'relative', overflow: 'hidden' }}>
+        <LogoLoop logos={techLogos} speed={120} direction="left" logoHeight={48} gap={40} pauseOnHover scaleOnHover fadeOut fadeOutColor="#ffffff" ariaLabel="Technology partners" />
       </div>
     </div>
   );
